76254cd31c014d1f4e3ab2d64780342b4e12c04d
Ed Baker <edwbaker@gmail.com>
2018-03-23 14:50:06 +0000

    Missed taxa.

--- AAO/AAO.php
+++ AAO/AAO.php
@@ -87,7 +87,7 @@ function AAO_consolidate() {
     $system["data"]["aao"][$key]["tree match"] = $tree_match;
     $rec_taxa[$system["data"]["aao"][$key]["tree name"]][] = $system["data"]["aao"][$key];
     if (!in_array($tree_taxon, $unique_taxa) {
-      $unique_taxa = array_merge($uique_taxa, array($tree_taxon);
+      $unique_taxa = array_merge($uique_taxa, array($tree_taxon));
     }
   }
   
